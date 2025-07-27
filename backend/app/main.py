28ee674ad686524e6466f4bb701430d2042b2d1d
cl-py <cvarn3@uic.edu>
2025-07-26 21:31:20 -0500

    createPlaylist now searches the db before querying api

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask import request,jsonify
 import json
+from db import get_db
 from requests import get,post,put
 
 app = Flask(__name__)
@@ -39,14 +40,21 @@ def createPlaylist():
 
     for word in words:
         print(word)
+
         #check db
-        #if db returns something
-            #continue
-        #else
+        db = get_db()
+        song_matches_from_db = db.execute("SELECT name FROM songs WHERE name = ?"),
+        (word,).fetchall()
+
+        if song_matches_from_db is not None:
+            continue
+
+        else:
             #call the api
-        print(calls.search_for_song(word))
-            #if api returns something
-                #continue
+            print(calls.search_for_song(word))
+            returned_songs = calls.search_for_song(word)
+            if returned_songs is not None:
+                continue
             #else
                 #go up to next node to include the following in search
                 #if we go the whole phrase unable to find a match
