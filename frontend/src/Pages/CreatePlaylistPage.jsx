5ebda8989d6bd811ce51a2ea8a726c12009049e8
Eddie <ed.murillo717@gmail.com>
2025-08-01 22:28:13 -0500

    adding text field in creation page

--- frontend/src/Pages/CreatePlaylistPage.jsx
+++ frontend/src/Pages/CreatePlaylistPage.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
+import {useNavigate} from "react-router-dom";
 
 const CreatePlaylistPage = () =>{
+
+    const [sentence, setSentence]=useState('');
+
+    let navigate = useNavigate();
+
+    const handleCreatePlaylist = () => {
+        let path = "/WaitingPage";
+
+        navigate(path);
+    };
+
     return (
-        <div></div>
+        <div>
+            <div className = "UpperSection"></div>
+            <div className = "textSection">
+                <input 
+                    type="text"
+                    value={sentence}
+                    onChange={(e)=> setSentence(e.target.value)}
+                    />
+                <button onClick={handleCreatePlaylist}>Create</button>
+            </div>
+        </div>
     )
 }
 
