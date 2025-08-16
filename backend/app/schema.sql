ec54b0d80c979653af5db4971e912decacc9e6bb
cl-py <cvarn3@uic.edu>
2025-08-16 14:33:41 -0500

    changes to schema to store album cover

--- backend/app/schema.sql
+++ backend/app/schema.sql
@@ -1,5 +1,12 @@
 CREATE TABLE songs(
     name TEXT NOT NULL,
     url TEXT NOT NULL,
+    album_name TEXT NOT NULL,
     id TEXT PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE covers(
+    album_name TEXT NOT NULL,
+    link TEXT NOT NULL,
+    FOREIGN KEY (album_name) REFERENCES songs(album_name)
 );
