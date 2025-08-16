22bcd6e1b937f03760ec1621148507bbea093b85
cl-py <cvarn3@uic.edu>
2025-08-16 14:34:40 -0500

    minor syntax

--- backend/app/schema.sql
+++ backend/app/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE songs(
     album_name TEXT NOT NULL,
     artist TEXT NOT NULL,
-    id TEXT PRIMARY KEY NOT NULL
+    id TEXT PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
     url TEXT NOT NULL
 );
