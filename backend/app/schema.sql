6f81a152f8b1c80ea1912bd204e1f3791deea6d7
cl-py <cvarn3@uic.edu>
2025-08-16 14:34:05 -0500

    small change

--- backend/app/schema.sql
+++ backend/app/schema.sql
@@ -1,12 +1,13 @@
 CREATE TABLE songs(
-    name TEXT NOT NULL,
-    url TEXT NOT NULL,
     album_name TEXT NOT NULL,
+    artist TEXT NOT NULL,
     id TEXT PRIMARY KEY NOT NULL
+    name TEXT NOT NULL,
+    url TEXT NOT NULL
 );
 
 CREATE TABLE covers(
     album_name TEXT NOT NULL,
-    link TEXT NOT NULL,
-    FOREIGN KEY (album_name) REFERENCES songs(album_name)
+    artist TEXT NOT NULL,
+    link TEXT NOT NULL
 );
