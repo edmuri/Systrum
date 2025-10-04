44a783ded4f43c15e669ef3e62efc16ba32fbc63
Eddie <ed.murillo717@gmail.com>
2025-10-04 15:28:56 -0500

    removing excess files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,6 @@ This is going to contain all the different possible pages
 import LandingPage from "./Pages/LandingPage";
 import CreatePlaylistPage from "./Pages/CreatePlaylistPage";
 import PlaylistResultPage from "./Pages/PlaylistResultPage";
-import WaitingPage from "./Pages/WaitingPage";
-import CreatorsPage from "./Pages/CreatorsPage";
 import AboutPage from "./Pages/AboutPage";
 
 // import PlaylistPage from "./Pages/PlaylistPage";
@@ -21,10 +19,8 @@ function App(){
         <Routes>
             <Route path="/" element = {<LandingPage />} />
             <Route path="/CreatePlaylist" element = {<CreatePlaylistPage/>}/>
-            <Route path="/WaitingPage" element = {<WaitingPage/>}/>
             <Route path="/PlaylistResult" element = {<PlaylistResultPage/>}/>
             <Route path="/AboutPage" element = {<AboutPage />} />
-            <Route path="/CreatorsPage" element = {<CreatorsPage/>}/>
         </Routes>
         </BrowserRouter>
     );
